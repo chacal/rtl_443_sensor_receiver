6f9976e55d853fe4e464b874acfa20611ee40da1
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-02-17 21:10:29 +0200

    Publish RF switch events to MQTT broker

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const readline = require('readline')
 const express = require('express')
 const log = require('winston')
 const morgan = require('morgan')
+const mqtt = require('mqtt')
 
 const sensorCache = {}
 const sensorToInstanceMap = {  // Map rtl_433 'id' field to own sensor instances, see Evernote for instance bindings
@@ -10,9 +11,11 @@ const sensorToInstanceMap = {  // Map rtl_433 'id' field to own sensor instances
   167: 51,
   2:   52,
 }
+const MQTT_BROKER = 'mqtt://ha-opi'
 
 startRtl_433()
 startHttpServer()
+const mqttClient = startMqttClient(MQTT_BROKER)
 
 
 
@@ -36,6 +39,10 @@ function handleLine(line) {
 function handleInputJson(json) {
   if(json.model === 'WT450 sensor' || json.model === 'Nexus Temperature/Humidity') {
     handleWT450OrNexus(json)
+  } else if(json.model === 'Waveman Switch Transmitter') {
+    handleSwitchTransmitter(json)
+  } else {
+    console.log('Got unknown message', json)
   }
 }
 
@@ -48,6 +55,10 @@ function handleWT450OrNexus(json) {
   const t = { instance, tag: 't', temperature: json.temperature_C, ts: new Date() }
   const h = { instance, tag: 'h', humidity: json.humidity, ts: new Date() }
   sensorCache[instance] = { t, h }
+}
+
+function handleSwitchTransmitter(json) {
+  mqttClient.publish(`/switch/intertechno/${json.id}/${json.channel}/${json.button}/state`, json.state.toUpperCase(), { retain: true, qos: 1 })
 }
 
 
@@ -69,4 +80,16 @@ function startHttpServer() {
   app.listen(3000, function() {
     log.info('rtl_433 sensor receiver listening on port 3000')
   })
+}
+
+
+
+////  MQTT client for publishing events about RF switches
+
+function startMqttClient(brokerUrl) {
+  const client = mqtt.connect(brokerUrl)
+  client.on('connect', function () {
+    console.log("Connected to MQTT server..")
+  })
+  return client
 }
